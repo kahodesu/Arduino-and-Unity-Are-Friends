d6f0c17deef9cbc2c61d836abe81105fb0950649
kaho です! <Bucky@10-253-0-75.DYNAPOOL.NYU.EDU>
2018-05-01 14:52:18 -0400

    initial commit

---
+++ Assets/ArduinoConnector.cs
@@ -0,0 +1,100 @@
+﻿/* Modded version of ArduinoConnector by Alan Zucconi
+ * http://www.alanzucconi.com/?p=2979
+ * changed data read and written to be a byte. 
+ */
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+
+
+public class ArduinoConnector : MonoBehaviour {
+
+
+    /* The serial port where the Arduino is connected. */
+    [Tooltip("The serial port where the Arduino is connected")]
+	public string port = "/dev/cu.usbmodem1411";
+    /* The baudrate of the serial port. */
+    [Tooltip("The baudrate of the serial port")]
+    public int baudrate = 115200;
+    private SerialPort stream;
+    
+	byte[] b = new byte[1]; //this is a byte array called b with one item.
+
+    public void Open () {
+        // Opens the serial port
+        stream = new SerialPort(port, baudrate);
+        stream.ReadTimeout = 50;
+		//Debug.Log("opening...");
+        stream.Open();
+		
+        //this.stream.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+    }
+    
+
+    public void WriteToArduino(byte message)
+     {
+       //  Debug.Log(message);
+       	 b[0] = message; //stick byte data into byte array b
+         stream.Write(b, 0, 1); //sends b with some other info.
+         stream.BaseStream.Flush(); 
+    }
+
+    public int ReadFromArduino(int timeout)
+    {
+        stream.ReadTimeout = timeout;
+        try
+        {
+			//Debug.Log("reading...");
+            return stream.ReadByte(); //return whatever is read here
+        }
+        catch (TimeoutException)
+        {            
+            return 0;
+        }
+    }
+    
+    public IEnumerator AsynchronousReadFromArduino(Action<string> callback, Action fail = null, float timeout = float.PositiveInfinity)
+    {
+        DateTime initialTime = DateTime.Now;
+        DateTime nowTime;
+        TimeSpan diff = default(TimeSpan);
+
+        string dataString = null;
+
+        do
+        {
+            // A single read attempt
+            try
+            {
+                dataString = stream.ReadLine();
+            }
+            catch (TimeoutException)
+            {
+                dataString = null;
+            }
+
+            if (dataString != null)
+            {
+                callback(dataString);
+                yield return null;
+            } else
+                yield return new WaitForSeconds(0.05f);
+
+            nowTime = DateTime.Now;
+            diff = nowTime - initialTime;
+
+        } while (diff.Milliseconds < timeout);
+
+        if (fail != null)
+            fail();
+        yield return null;
+    }
+
+    public void Close()
+    {
+        stream.Close();
+    }
+}
---
+++ Assets/CubeScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+	public ArduinoConnector arduinoConnector; //create an instance of object ArduinoConnector called arduinoConnector
+	public Renderer rend; //create an instance of object Renderer called rend
+ 	float timeLeft = 0;
+	int var;
+	bool yellow = false; //just have to make sure it starts the same as on the Arduino
+
+	void Start()
+	{		
+		arduinoConnector = GetComponent<ArduinoConnector>(); //to access properties of ArduinoConnector script
+		arduinoConnector.Open(); //open serial connection
+    	rend = GetComponent<Renderer>(); //to access the renderer of the object
+    	rend.material.SetColor("_Color",Color.blue); //set the color of the cube to BLUE
+	}
+
+	void Update()
+	{
+		
+		var = arduinoConnector.ReadFromArduino(1); //read from Arduino
+		
+		if (var == 1) {	 //If 1 is being sent from Arduino - button is being pressed!
+			Debug.Log("Button Pressed!");
+        	rend.material.SetColor("_Color",Color.red); //turn the cube RED
+        	timeLeft = .50f; //start the timer
+		} 
+
+		timeLeft -= Time.deltaTime; //counting down
+		if ( timeLeft < 0 ) //if time has run out
+     	{
+        rend.material.SetColor("_Color",Color.blue); //change the cube back to BLUE
+    	}	
+	}
+	
+	void OnMouseDown() //if the cube is clicked
+    {
+       if (!yellow){
+       		Debug.Log("Turn on LED!");
+       		rend.material.SetColor("_Color",Color.yellow); //turn the cube YELLOW
+       		arduinoConnector.WriteToArduino(255); //send 255 to Arduino
+       		yellow = true;
+		}
+		else {
+			Debug.Log("Turn LED off!");
+       		rend.material.SetColor("_Color",Color.blue); //turn the cube YELLOW
+       		arduinoConnector.WriteToArduino(255); //send 255 to Arduino
+       		yellow = false;
+		}
+    }	
+}
